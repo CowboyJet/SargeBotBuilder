3661e2a4fd2299f7ec0f26ba27da0adca77e9821
Dussen <XM65XI@europe.intranet>
2016-07-26 14:14:56 +0200

    inital hello world bot

---
+++ app.js
@@ -0,0 +1,33 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+//=========================================================
+// Bot Setup
+//=========================================================
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+  
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: "d8ea3167-7186-468b-b95b-edaf13b7a194",
+    appPassword: "xAafrPn7rUmbeeWx0KeaeB8"
+});
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+bot.dialog('/', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function (session, results) {
+        session.send('Hello %s!', results.response);
+    }
+]);
